523a0f528f5e2b152bb3192190480f19499f5f20
RakshitGumber <gumberrakshit@gmail.com>
2026-04-20 23:33:12 +0530

    added pong route

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello world!" }));
+    let app = Router::new()
+    .route("/", get(|| async { "Hello world!" }))
+    .route("/ping", get(||async {"pong"}));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     println!("Listening on http://127.0.0.1:3000");
