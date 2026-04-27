9f1ca08cdc8b2ff5cfdc8a24b9aa89e366fea029
RakshitGumber <gumberrakshit@gmail.com>
2026-04-27 09:05:59 +0530

    trying to make a logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use chrono::Local;
+use std::fs::File;
+use std::io::{self, Write};
+
+fn formatted_time_entry() -> String {
+    let now = Local::now();
+    let time_str = now.format("%a, %b %d %Y %I:%M:%S %p\n").to_string();
+    time_str
+}
+
+fn logger(filename: &str, string: &[u8]) -> io::Result<()> {
+    let mut f = File::create(filename)?;
+    f.write_all(string)?;
+    Ok(())
+}
+
+fn log(filename: &str) -> io::Result<()> {
+    let time_str = formatted_time_entry();
+    logger(filename, time_str.as_bytes())?;
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    log("log.txt").expect("Unable to write")
 }
