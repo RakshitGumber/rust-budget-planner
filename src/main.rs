bc4b223893565e2f3cb685525b4d7cef07f46aa4
RakshitGumber <gumberrakshit@gmail.com>
2026-04-27 08:07:20 +0530

    tried writing to the file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::fs::File;
-use std::io;
+use std::io::{self, Write};
 
-fn main() -> io::Result<()> {
-    let mut f = File::create("foo.txt")?;
+fn logger(filename: &str, string: &str) -> io::Result<()>{
+    let mut f = File::create(filename)?;
+    f.write_all(string.as_bytes())?;
     Ok(())
+}
+
+fn main() {
+    logger("log.txt", "It's working").expect("Unable to write")
 }
